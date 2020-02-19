b3137e2cbaaf59235a5e6664586808f077cac279
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-02-19 07:18:38 +0100

    Adding config file

---
+++ config.py
@@ -0,0 +1,28 @@
+# I use Tweepy, you can find all the links and documentations on the Github repo,
+# Config file, I "import os" to use "os.getenv()" to get environment variables
+# I don't want to share my API and Access Token keys with everyone
+# using "import logging" for error logs
+
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+def create_api():
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
