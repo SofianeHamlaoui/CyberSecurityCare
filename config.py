c01d76b43e97c4dc86d99a7e9ec2be529ce40080
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-02-19 07:53:46 +0100

    adding comments to config.py

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
-# I use Tweepy, you can find all the links and documentations on the Github repo,
-# Config file, I "import os" to use "os.getenv()" to get environment variables
-# I don't want to share my API and Access Token keys with everyone
-# using "import logging" for error logs
+# Config.py file
+#Author  : 	SofianeHamlaoui
+#Github  : 	https://github.com/SofianeHamlaoui
+#Twitter : 	https://twitter.com/S0fianeHamlaoui
+
 # Set EnvVar : export $VARNAME=""
-import tweepy
-import logging
-import os
+
+import tweepy # I use Tweepy, you can find all the links and documentations on the Github repo,
+import logging # using "import logging" for error logs
+import os # I "import os" to use "os.getenv()" to get environment variables
 
 logger = logging.getLogger()
 
 def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    consumer_key = os.getenv("CONSUMER_KEY") # export CONSUMER_KEY="KEY"
+    consumer_secret = os.getenv("CONSUMER_SECRET") # export CONSUMER_SECRET="KEY"
+    access_token = os.getenv("ACCESS_TOKEN") # export ACCESS_TOKEN="KEY"
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET") # export ACCESS_TOKEN_SECRET="KEY"
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
